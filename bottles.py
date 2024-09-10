3322464493e31d13e371dd28933f8a229bccd66e
ppattyy <galla355@msu.edu>
2024-09-10 11:28:10 -0400

    Add files via upload

---
+++ bottles.py
@@ -0,0 +1,16 @@
+beverage = "sprite"
+count = 2
+
+print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage)
+print("Take one down, pass it around")
+count = count - 1
+print(count, "bottles of", beverage, "on the wall")
+
+print("")
+
+print(count, "bottles of", beverage, "on the wall")
+print(count, "bottles of", beverage)
+print("Take one down, pass it around")
+count = count - 1
+print(count, "bottles of", beverage, "on the wall")
